c8281a776332e392d9131fa935266cdf44cb2457
Charles Tyra <charlestyra@MacBook-Pro-3.lan>
2023-02-18 16:09:14 -0500

    migrating functionality to Express Routing

--- backend/routes/api/tweets.js
+++ backend/routes/api/tweets.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Respond with a user resource, poopy');
+router.get('/', function (req, res, next) {
+   res.json({
+      message: "GET /api/tweets"
+   });
 });
 
 module.exports = router;
